1f5e534dd01eac71bf713f6e8d2a0c26135e5afc
Luca Andaloro <luca.andaloro4@gmail.com>
2022-06-14 16:52:29 +0200

    Necessary models

---
+++ database/factories/HouseImageFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\HouseImage;
+use Faker\Generator as Faker;
+
+$factory->define(HouseImage::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
