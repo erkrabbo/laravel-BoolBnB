3ba27547eb9fbae8290d696cb1914d27f654ab1f
Piergiorgio Piras <piraspiergiorgio5@gmail.com>
2022-06-17 17:06:11 +0200

    app

--- resources/views/houses/show.blade.php
+++ resources/views/houses/show.blade.php
@@ -16,7 +16,7 @@
                     @endforeach
                     <h4 class="pt-2">Proprietario: {{ $user->name }}</h4>
                     
-                    <a class="btn btn-primary mt-4" href="{{ route('houses.edit') }}"><span class="text-white">Modifica l'inserzione</span></a>
+                    {{-- <a class="btn btn-primary mt-4" href="{{ route('houses.edit') }}"><span class="text-white">Modifica l'inserzione</span></a> --}}
                     {{-- <a class="btn btn-primary mt-4" href="{{ route('houses.destroy') }}"><span class="text-white">Elimina l'inserzione</span></a> --}}
 
                 </div>
