d6b1a7a6480999db05ecf902086e0c2f31390f2e
Piergiorgio Piras <piraspiergiorgio5@gmail.com>
2022-06-28 11:49:28 +0200

    ops

--- database/seeds/HouseSponsorizationSeeder.php
+++ database/seeds/HouseSponsorizationSeeder.php
@@ -15,11 +15,12 @@ class HouseSponsorizationSeeder extends Seeder
     {
         $houses = House::inRandomOrder()->limit(20)->get();
         foreach ($houses as $house) {
-          $now = new DateTime(dateBetween('Y-m-d H:i:s'));
+          $now = new DateTime(date('Y-m-d H:i:s'));
           $rand = rand(1, 3);
           $sponsorization = Sponsorization::find($rand);
           $expiration = (clone $now)->add(new DateInterval("PT{$sponsorization->duration}H"));
           $house -> sponsorizations() -> attach([$rand => ['created_at' => $now, 'expiration_date' => $expiration]]);
+
         }
     }
 }
